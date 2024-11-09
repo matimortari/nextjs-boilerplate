7ccf62b9911ef733994827268d2f006f03ee539a
matheus <matheus.felipe.19rt@gmail.com>
2024-11-09 18:21:48 -0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { useQuery } from "@tanstack/react-query"
+
+const getMessage = async () => {
+	const response = await fetch("/api/hello")
+	if (!response.ok) throw new Error("Failed to fetch data")
+	return response.json()
+}
+
+export default function Home() {
+	const { data, refetch } = useQuery({
+		queryKey: ["get-message"],
+		queryFn: getMessage,
+		enabled: false
+	})
+
+	return (
+		<div className="m-8 flex h-screen flex-col items-center">
+			<button className="button" onClick={() => refetch()}>
+				Hello World!
+			</button>
+
+			{data?.message && (
+				<div className="m-4">
+					<strong>{data.message}</strong>
+				</div>
+			)}
+		</div>
+	)
+}
